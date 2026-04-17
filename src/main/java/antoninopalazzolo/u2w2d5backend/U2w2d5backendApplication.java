63f559ef9368ffba67d4d4df1aabc77592cdfba6
palax-99 <ninociao7@gmail.com>
2026-04-17 10:08:31 +0200

    Initial commit

---
+++ src/main/java/antoninopalazzolo/u2w2d5backend/U2w2d5backendApplication.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.u2w2d5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w2d5backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w2d5backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antoninopalazzolo/u2w2d5backend/U2w2d5backendApplicationTests.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.u2w2d5backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w2d5backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
